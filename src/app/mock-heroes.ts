52d20b7dc15b60b7cbd3ea66bffccc956ad321be
FelipeZuriel <felipezuriel@hotmail.com.br>
2023-02-20 17:17:39 -0300

    aiai

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 10, name: 'Dr. Kaua Aua' },
+  { id: 11, name: 'Pato Jonathan' },
+  { id: 12, name: 'Cornnos' },
+  { id: 13, name: 'Magneta' },
+  { id: 14, name: 'RubberMan' },
+  { id: 15, name: 'Dynama' },
+  { id: 16, name: 'Dr. IQ' },
+  { id: 17, name: 'Magma' },
+  { id: 18, name: 'Tornado' }
+];
